b3d05e196e9796753709170101d5fc8ea76ca000
aluno <aristeutrassante@gmail.com>
2019-09-12 11:26:32 -0300

    com alert

--- app/src/main/java/com/example/projetotcc10/Controle/Listar_Admin.java
+++ app/src/main/java/com/example/projetotcc10/Controle/Listar_Admin.java
@@ -1,7 +1,9 @@
 package com.example.projetotcc10.Controle;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
@@ -23,6 +25,9 @@ public class Listar_Admin extends AppCompatActivity {
     private ListView listaAdmin;
     private List<Admin> admins;
     private Button aliasCadastrarAdmin;
+
+    private AlertDialog alerta;
+
 
 
     private String[] ArrayAdmins = new String[]{"Joao\njoao@gmail.com","Lucas\n" +
@@ -61,20 +66,43 @@ public class Listar_Admin extends AppCompatActivity {
             @Override
             public boolean onItemLongClick(AdapterView<?> parent, View view,
                                            int position, long id) {
-                // TODO Auto-generated method stub
 
 
 
              listaAdmin.getItemAtPosition(position);
+             exemplo_simples();
 
-                Toast.makeText(getApplicationContext(), "Item Deleted", Toast.LENGTH_LONG).show();
+
 
                 return true;
             }
 
         });
         }
-
+    private void exemplo_simples() {
+        //Cria o gerador do AlertDialog
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        //define o titulo
+        builder.setTitle("Alerta!");
+        //define a mensagem
+        builder.setMessage("Deseja mesmo excluir esse administrador?");
+        //define um botão como positivo
+        builder.setPositiveButton("Sim", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface arg0, int arg1) {
+                Toast.makeText(getApplicationContext(), "Administrador excluido" , Toast.LENGTH_SHORT).show();
+            }
+        });
+        //define um botão como negativo.
+        builder.setNegativeButton("Não", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface arg0, int arg1) {
+                Toast.makeText(getApplicationContext(), "Ação cancelada" , Toast.LENGTH_SHORT).show();
+            }
+        });
+        //cria o AlertDialog
+        alerta = builder.create();
+        //Exibe
+        alerta.show();
+    }
     @Override
     public boolean onOptionsItemSelected(MenuItem item) { //Botão adicional na ToolBar
         switch (item.getItemId()) {
